086615cf23dbd2b6784b1edfcc94fabe66e0792c
josephaddie54-afk <josephaddie54@gmail.com>
2026-03-25 13:55:11 +0800

    Update AgeCheck.java

--- exercises/exercise06_logic/AgeCheck.java
+++ exercises/exercise06_logic/AgeCheck.java
@@ -5,7 +5,7 @@ public class AgeCheck {
         int age = 17;
 
         // Task: Fix the error by using '==' for comparison instead of '=' for assignment.
-        if (age = 18) {
+        if (age == 18) {
             System.out.println("You are an adult!");
         } else {
             System.out.println("You are still a minor.");
