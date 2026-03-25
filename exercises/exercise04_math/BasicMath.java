279115fb7a73f24c029639edadc16bd5c49fcd63
josephaddie54-afk <josephaddie54@gmail.com>
2026-03-25 13:43:49 +0800

    Update BasicMath.java

--- exercises/exercise04_math/BasicMath.java
+++ exercises/exercise04_math/BasicMath.java
@@ -4,7 +4,7 @@ public class BasicMath {
     public static void main(String[] args) {
         // Task: Fix the error by using the correct type or rounding.
         // Hint: 5.5 is a double, not an int!
-        int score = 5.5;
+        double score = 5.5;
 
         System.out.println("Your score is: " + score);
     }
